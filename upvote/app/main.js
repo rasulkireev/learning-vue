13cbd866c998b2bb777540f24e1a0d3fb361964b
Rasul Kireev <me@rasulkireev.com>
2020-02-07 14:18:13 -0500

    upvote is done

--- upvote/app/main.js
+++ upvote/app/main.js
@@ -1,3 +1,39 @@
+const submissionComponent = {
+    template:
+    ` <div class="w-full flex">
+        <figure class="flex-grow-0 flex-shrink-0 block mr-4">
+            <img class="w-20" :src="submission.submissionImage">
+        </figure>
+        
+        <div class="mr-6 flex-grow flex-shrink text-left">
+            <h3>
+                <a :href="submission.url" class="text-blue-800 font-bold text-lg">{{ submission.title }}</a>
+                <span class="text-grey-700 p-1 text-xs py-1 px-2 bg-gray-300 rounded ml-1">#{{ submission.id }}</span>
+            </h3>
+            <p class="mb-1 text-gray-800">{{ submission.description }}</p>
+            <small class="text-sm">
+                Submitted by: <img class="inline w-8 rounded-full" :src="submission.avatar">
+            </small>
+        </div>
+        
+        <div class="flex-grow-0 flex-shrink-0">
+            <span class="" @click="upvote(submission.id)">
+            <i class="las la-chevron-up"></i>
+            <strong class="">{{ submission.votes }}</strong>
+            </span>
+        </div>
+    </div>`,
+    props: ['submission', 'submissions'],
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find(
+                submission => submission.id === submissionId
+            );
+            submission.votes++;
+        }
+    },
+};
+
 new Vue({
     el: '#app',
     data: {
@@ -10,12 +46,7 @@ new Vue({
             });
         }
     },
-    methods: {
-        upvote(submissionId) {
-            const submission = this.submissions.find(
-                submission => submission.id === submissionId
-            );
-            submission.votes++;
+    components: {
+        'submission-component': submissionComponent
         }
-    }
 });
