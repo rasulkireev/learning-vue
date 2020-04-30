2f66dd5c53bf0700348bfd0de7595d6a0e41c269
Rasul Kireev <me@rasulkireev.com>
2020-04-30 13:19:54 -0400

    Finished Chapter 2

--- 02a-calendar_app/src/app/store.js
+++ 02a-calendar_app/src/app/store.js
@@ -9,11 +9,63 @@ export const store = {
         return this.state.seedData.find(day => day.active);
     },
 
+
     setActiveDay(dayId) {
         this.state.seedData.map(dayObj => {
             dayObj.id === dayId
-                ? (dayObj.active = true)
-                : (dayObj.active = false);
+            ? (dayObj.active = true)
+            : (dayObj.active = false);
         });
-    }
+    },
+
+    submitEvent (eventDetails) {
+        const activeDay = this.getActiveDay();
+        activeDay.events.push({ 'details': eventDetails, 'edit': false });
+    },
+
+    editEvent (dayId, eventDetails) {
+        this.resetEditOfAllEvents();
+        const eventObj = this.getEventObj(dayId, eventDetails);
+        eventObj.edit = true;
+    },
+
+
+    updateEvent (dayId, originalEventDetails, newEventDetails) {
+        // Find the day object
+        const eventObj = this.getEventObj(dayId, originalEventDetails);
+
+        // Set the event details to the new details
+        // and turn off editing
+        eventObj.details = newEventDetails;
+        eventObj.edit = false;
+    },
+
+    deleteEvent (dayId, eventDetails) {
+        const dayObj = this.state.seedData.find(
+            day => day.id === dayId
+        );
+        const eventIndexToRemove = dayObj.events.findIndex(
+            event => event.details === eventDetails
+        );
+        dayObj.events.splice(eventIndexToRemove, 1);
+    },
+
+
+    // Helper functions
+    getEventObj (dayId, eventDetails) {
+        const dayObj = this.state.seedData.find(
+            day => day.id === dayId
+        );
+        return dayObj.events.find(
+            event => event.details === eventDetails
+        );
+    },
+
+    resetEditOfAllEvents () {
+        this.state.seedData.map((dayObj) => {
+            dayObj.events.map((event) => {
+                event.edit = false;
+            });
+        });
+    },
 }
