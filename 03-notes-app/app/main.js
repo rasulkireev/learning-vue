a5e9a8d1b2f8354b43edaf5a48eae3365a8cc7eb
Rasul Kireev <me@rasulkireev.com>
2020-05-05 14:48:05 -0400

    Finished vuex-notes-app. Started shopping-cart

--- 03-notes-app/app/main.js
+++ 03-notes-app/app/main.js
@@ -1,3 +1,5 @@
+const EventBus = new Vue();
+
 const inputComponent = {
     template: `<input
     :placeholder='placeholder'
@@ -12,14 +14,29 @@ const inputComponent = {
     props: ['placeholder'],
     methods: {
         monitorEnterKey() {
-            this.$emit('add-note', {
+            EventBus.$emit('add-note', {
                 note: this.input,
                 timestamp: new Date().toLocaleString()
             });
             this.input = '';
         },
     }
-}
+};
+
+// Note count component
+const noteCountComponent = {
+    template:
+    `<div class="note-count">Note count: <strong>{{ noteCount }}</strong></div>`,
+    data () {
+        return {
+            noteCount: 0
+        }
+    },
+    created () {
+        EventBus.$on('add-note', event => this.noteCount++);
+    },
+
+};
 
 new Vue({
     el: '#app',
@@ -28,13 +45,23 @@ new Vue({
         timestamps: [],
         placeholder: 'Enter a note'
     },
+
     components: {
-        'input-component': inputComponent
+        'input-component': inputComponent,
+        'note-count-component': noteCountComponent
     },
+
+    created () {
+        EventBus.$on('add-note', event => this.addNote(event));
+    },
+
     methods: {
         addNote(event) {
             this.notes.push(event.note);
             this.timestamps.push(event.timestamp);
-        }
+        },
+        updateCount() {
+            this.count++;
+        },
     },
 })
